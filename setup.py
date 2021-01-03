27a4895a5748c8c61ad7f1756e01623569391e74
Calen Pennington <cale@edx.org>
2021-01-03 09:50:32 -0500

    Lang level 3

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("_rebbl_predictor.pyx")
+    ext_modules = cythonize("_rebbl_predictor.pyx", language_level="3")
 )
